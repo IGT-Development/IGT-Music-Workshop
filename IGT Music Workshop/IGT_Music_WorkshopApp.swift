5c82f37e725efa6c6c2b1bd668f3068c6fe3ffda
Dusha <56073470+KayllenZzZ@users.noreply.github.com>
2024-01-14 10:32:00 +0300

    Initial Commit

---
+++ IGT Music Workshop/IGT_Music_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  IGT_Music_WorkshopApp.swift
+//  IGT Music Workshop
+//
+//  Created by Андрей Чередник on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IGT_Music_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
